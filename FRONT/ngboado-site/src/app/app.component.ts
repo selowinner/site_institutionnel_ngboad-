bd9b3053bf0bbdf7dbefab020515e399f75a4353
salma-mindy <shellyaka998@gmail.com>
2021-05-12 09:12:18 +0000

    Ajout du loader dans index.html et style.css

--- FRONT/ngboado-site/src/app/app.component.ts
+++ FRONT/ngboado-site/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -6,6 +6,25 @@ declare var $: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngboado-site';
+
+  public ngOnInit(): void {
+    $(document).ready(function () {
+      "use strict";
+
+      // here all ready functions
+      loader();
+    });
+
+    /* ===============================================
+      1. PRELOADER
+    =============================================== */
+    function loader() {
+      "use strict";
+      setTimeout(function () {
+          $('#loader-wrapper').fadeOut();
+      }, 5500);
+    };
+  }
 }
