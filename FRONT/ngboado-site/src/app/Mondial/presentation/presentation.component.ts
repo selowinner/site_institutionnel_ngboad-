f9aca13f460f3b8be1db565e0130eb42ddb73130
salma-mindy <shellyaka998@gmail.com>
2021-06-19 16:17:03 +0000

    Mise à jour presentation

--- FRONT/ngboado-site/src/app/Mondial/presentation/presentation.component.ts
+++ FRONT/ngboado-site/src/app/Mondial/presentation/presentation.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 declare const openMenu: any;
 declare const closeMenu: any;
+declare const showMoreTexte: any;
+declare const displayAndHideImage: any;
 
 @Component({
   selector: 'app-presentation',
@@ -12,6 +14,21 @@ export class PresentationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayImage(activeElmt) {
+    var historique = document.getElementById('logoHitorique');
+    var assembleGe = document.getElementById('logoAssGen');
+    var conseilAdm = document.getElementById('conseilAdmin');
+    var secretarialGen = document.getElementById('secretarialGen');
+    var cabMak = document.getElementById('cabMak');
+    var conEco = document.getElementById('conEco');
+    var courUniverselle = document.getElementById('courUniverselle');
+    displayAndHideImage(historique, assembleGe, conseilAdm, secretarialGen, cabMak, conEco, courUniverselle, activeElmt);
+  }
+
+  showTexte(elementId, btnId) { 
+    showMoreTexte(elementId, btnId);
   }
   openClickMenu() { openMenu();}
   closeClickMenu(){closeMenu();}
--- FRONT/ngboado-site/src/assets/js/custom.js
+++ FRONT/ngboado-site/src/assets/js/custom.js
@@ -66,10 +66,6 @@ function myFunction() {
     }
   }
 
-
-
-
-
   function getSuggestionForm(suggElem, plainteElem, autreElem){
     suggElem.style.display = 'block';
     plainteElem.style.display = 'none';
@@ -87,3 +83,75 @@ function myFunction() {
       plainteElem.style.display = 'none';
       autreElem.style.display = 'block';
   }
+
+  function displayAndHideImage(historiqueId, assembleId, conseilAdminId, secretarialGenId, cabinetMaketingId, conseilEcoId, courUnivId, activeElementId){
+    if(activeElementId === historiqueId.id){
+      historiqueId.style.display = 'block';
+      assembleId.style.display = 'none';
+      conseilAdminId.style.display = 'none';
+      secretarialGenId.style.display = 'none';
+      cabinetMaketingId.style.display = 'none';
+      conseilEcoId.style.display = 'none';
+      courUnivId.style.display = 'none';
+    }else if(activeElementId === assembleId.id){
+      historiqueId.style.display = 'none';
+      assembleId.style.display = 'block';
+      conseilAdminId.style.display = 'none';
+      secretarialGenId.style.display = 'none';
+      cabinetMaketingId.style.display = 'none';
+      conseilEcoId.style.display = 'none';
+      courUnivId.style.display = 'none';
+    }else if(activeElementId === conseilAdminId.id){
+      historiqueId.style.display = 'none';
+      assembleId.style.display = 'none';
+      conseilAdminId.style.display = 'block';
+      secretarialGenId.style.display = 'none';
+      cabinetMaketingId.style.display = 'none';
+      conseilEcoId.style.display = 'none';
+      courUnivId.style.display = 'none';
+    }else if(activeElementId === secretarialGenId.id){
+      historiqueId.style.display = 'none';
+      assembleId.style.display = 'none';
+      conseilAdminId.style.display = 'none';
+      secretarialGenId.style.display = 'block';
+      cabinetMaketingId.style.display = 'none';
+      conseilEcoId.style.display = 'none';
+      courUnivId.style.display = 'none';
+    }else if(activeElementId === cabinetMaketingId.id){
+      historiqueId.style.display = 'none';
+      assembleId.style.display = 'none';
+      conseilAdminId.style.display = 'none';
+      secretarialGenId.style.display = 'none';
+      cabinetMaketingId.style.display = 'block';
+      conseilEcoId.style.display = 'none';
+      courUnivId.style.display = 'none';
+    }else if(activeElementId === conseilEcoId.id){
+      historiqueId.style.display = 'none';
+      assembleId.style.display = 'none';
+      conseilAdminId.style.display = 'none';
+      secretarialGenId.style.display = 'none';
+      cabinetMaketingId.style.display = 'none';
+      conseilEcoId.style.display = 'block';
+      courUnivId.style.display = 'none';
+    }else{
+      historiqueId.style.display = 'none';
+      assembleId.style.display = 'none';
+      conseilAdminId.style.display = 'none';
+      secretarialGenId.style.display = 'none';
+      cabinetMaketingId.style.display = 'none';
+      conseilEcoId.style.display = 'none';
+      courUnivId.style.display = 'block';
+    }
+  }
+
+  function showMoreTexte(elemId, btnLirePlus){
+    var elStyle = document.getElementById(`${elemId}`);
+    var buttonElm = document.getElementById(`${btnLirePlus}`);
+    if(elStyle.style.display === 'none'){
+        elStyle.style.display = 'block';
+        buttonElm.textContent = "Masquer"
+    }else{
+        elStyle.style.display = 'none';
+        buttonElm.textContent = "Lire la suite"
+    }
+}
