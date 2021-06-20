a6ce3d900a6fd4ba125432e3dcefa4f83583facb
salma-mindy <shellyaka998@gmail.com>
2021-06-20 19:09:53 +0000

    Animation menu v1

--- FRONT/ngboado-site/src/app/District/presentation-district/presentation-district.component.ts
+++ FRONT/ngboado-site/src/app/District/presentation-district/presentation-district.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 declare const openMenu: any;
 declare const closeMenu: any;
+declare const showMoreTexte: any;
+declare const displayAndHideImage: any;
+
 @Component({
   selector: 'app-presentation-district',
   templateUrl: './presentation-district.component.html',
@@ -11,6 +14,20 @@ export class PresentationDistrictComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  displayImage(activeElmt) {
+    var historique = document.getElementById('logoHitorique');
+    var assembleGe = document.getElementById('logoAssGen');
+    var conseilAdm = document.getElementById('conseilAdmin');
+    var secretarialGen = document.getElementById('secretarialGen');
+    var cabMak = document.getElementById('cabMak');
+    var conEco = document.getElementById('conEco');
+    var courUniverselle = document.getElementById('courUniverselle');
+    displayAndHideImage(historique, assembleGe, conseilAdm, secretarialGen, cabMak, conEco, courUniverselle, activeElmt);
+  }
+
+  showTexte(elementId, btnId) { 
+    showMoreTexte(elementId, btnId);
   }
   openClickMenu() { openMenu();}
   closeClickMenu(){closeMenu();}
