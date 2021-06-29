e17dce6f77c0ba9ded420a52530ecf06936241e0
salma-mindy <shellyaka998@gmail.com>
2021-06-29 18:35:13 +0000

    Update regional v29062021

--- FRONT/ngboado-site/src/app/Regional/Mission/reinsertion-programme-regional/reinsertion-programme-regional.component.ts
+++ FRONT/ngboado-site/src/app/Regional/Mission/reinsertion-programme-regional/reinsertion-programme-regional.component.ts
@@ -9,7 +9,7 @@ declare const $: any;
   styleUrls: ['./reinsertion-programme-regional.component.css']
 })
 export class ReinsertionProgrammeRegionalComponent implements OnInit {
-
+  number = 1;
   constructor() { }
 
   public ngOnInit(): void {
@@ -21,26 +21,48 @@ export class ReinsertionProgrammeRegionalComponent implements OnInit {
         $('#autoWidth').removeClass('cS-hidden');
       }
     });
+  }
 
-    $('#programNonInst').lightSlider({
-      autoWidth: true,
-      loop: true,
-      onSliderLoad: function() {
-        $('#programNonInst').removeClass('cS-hidden');
-      }
-    });
+  dispayProgrammeNotInst() {
+    var element = document.getElementById('slideMission');
+    var elStyle = document.getElementById('ProgNInst');
+    var elRmStyle = document.getElementById('ProgInst');
+    var prInst = document.getElementById('projetInstitutionel');
+    if(element.style.display === 'none'){
+      //console.log(this.number == 1);
+      element.style.display = 'block';
+      elStyle.style.background = 'linear-gradient(to right, #d20514, #d20514)';
+      elStyle.style.color = '#fff';
+      elStyle.style.border = 'none';
+      
+      elRmStyle.style.background = '#fff';
+      elRmStyle.style.color = '#4d4d4d';
+      elRmStyle.style.border = '1px solid #ced4da !important';
 
-    document.getElementById("ProgNInst").addEventListener("click", function() {
-      const x = document.getElementById("hiddenSlide");
-      console.log(x.style.visibility);
-      if(x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
+      prInst.style.display = 'none';
+      if(this.number == 1){
+        $('#AutoWidth').lightSlider({
+          autoWidth: true,
+          loop: true,
+          onSliderLoad: function() {
+            $('#AutoWidth').removeClass('cS-hidden');
+          }
+        });
       }
-    }); 
-    
-}
+      this.number = this.number + 1;
+    }else{
+      element.style.display = 'none';
+      elStyle.style.background = '#fff';
+      elStyle.style.color = '#4d4d4d';
+      elStyle.style.border = '1px solid #ced4da !important';
+
+      elRmStyle.style.background = 'linear-gradient(to right, #d20514, #d20514)';
+      elRmStyle.style.color = '#fff';
+      elRmStyle.style.border = 'none';
+
+      prInst.style.display = 'block';
+    }
+  }
 
 openClickMenu() { openMenu();}
 
--- FRONT/ngboado-site/src/app/Regional/blog-regional/blog-regional.component.ts
+++ FRONT/ngboado-site/src/app/Regional/blog-regional/blog-regional.component.ts
@@ -15,16 +15,6 @@ export class BlogRegionalComponent implements OnInit {
   
   public ngOnInit(): void {
     $(document).ready(function() {
-      $('#prNotInst').lightSlider({
-        item:1,
-        slideMargin:0,
-        slidePager: false,
-        slideWidth:200,
-        loop:true,
-        controls:false,
-        pager:false
-      });
-
       var slider = $('#programNonInst').lightSlider({
         item:2,
         slideMargin:0,
@@ -39,51 +29,11 @@ export class BlogRegionalComponent implements OnInit {
         slider.goToPrevSlide(); 
       });
 
-      $('#goToPrevSlide1').click(function(){
-        slider.goToPrevSlide(); 
-      });
-
-      $('#goToPrevSlide2').click(function(){
-        slider.goToPrevSlide(); 
-      });
-
-      $('#goToPrevSlide3').click(function(){
-        slider.goToPrevSlide(); 
-      });
-
-      $('#goToPrevSlide4').click(function(){
-        slider.goToPrevSlide(); 
-      });
-
-      $('#goToPrevSlide5').click(function(){
-        slider.goToPrevSlide(); 
-      });
-
-
       //Next
       $('#goToNextSlide').click(function(){
         slider.goToNextSlide(); 
       });
 
-      $('#goToNextSlide1').click(function(){
-        slider.goToNextSlide(); 
-      });
-
-      $('#goToNextSlide2').click(function(){
-        slider.goToNextSlide(); 
-      });
-
-      $('#goToNextSlide3').click(function(){
-        slider.goToNextSlide(); 
-      });
-
-      $('#goToNextSlide4').click(function(){
-        slider.goToNextSlide(); 
-      });
-
-      $('#goToNextSlide5').click(function(){
-        slider.goToNextSlide(); 
-      });
 
       //VIDEO DANS LES SLIDE
       var $videoSrc;  
