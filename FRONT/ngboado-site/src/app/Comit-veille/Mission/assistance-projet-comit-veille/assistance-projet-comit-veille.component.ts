4803769a3732ce238eea0b733ba7d052465838ca
salma-mindy <shellyaka998@gmail.com>
2021-06-29 11:19:56 +0000

    Update comite de veille v27062021

--- FRONT/ngboado-site/src/app/Comit-veille/Mission/assistance-projet-comit-veille/assistance-projet-comit-veille.component.ts
+++ FRONT/ngboado-site/src/app/Comit-veille/Mission/assistance-projet-comit-veille/assistance-projet-comit-veille.component.ts
@@ -10,7 +10,7 @@ declare const $: any;
   styleUrls: ['./assistance-projet-comit-veille.component.css']
 })
 export class AssistanceProjetComitVeilleComponent implements OnInit {
-
+  number = 1;
   constructor() { }
 
   public ngOnInit(): void {
@@ -21,14 +21,47 @@ export class AssistanceProjetComitVeilleComponent implements OnInit {
         $('#autoWidth').removeClass('cS-hidden');
       }
     });
+  }
 
-    $('#AutoWidth').lightSlider({
-      autoWidth: true,
-      loop: true,
-      onSliderLoad: function() {
-        $('#AutoWidth').removeClass('cS-hidden');
+  dispayProgrammeNotInst() {
+    var element = document.getElementById('slideMission');
+    var elStyle = document.getElementById('ProgNInst');
+    var elRmStyle = document.getElementById('ProgInst');
+    var prInst = document.getElementById('projetInstitutionel');
+    if(element.style.display === 'none'){
+      //console.log(this.number == 1);
+      element.style.display = 'block';
+      elStyle.style.background = 'linear-gradient(to right, #ce8149, #ce8149)';
+      elStyle.style.color = '#fff';
+      elStyle.style.border = 'none';
+      
+      elRmStyle.style.background = '#fff';
+      elRmStyle.style.color = '#4d4d4d';
+      elRmStyle.style.border = '1px solid #ced4da !important';
+
+      prInst.style.display = 'none';
+      if(this.number == 1){
+        $('#AutoWidth').lightSlider({
+          autoWidth: true,
+          loop: true,
+          onSliderLoad: function() {
+            $('#AutoWidth').removeClass('cS-hidden');
+          }
+        });
       }
-    });
+      this.number = this.number + 1;
+    }else{
+      element.style.display = 'none';
+      elStyle.style.background = '#fff';
+      elStyle.style.color = '#4d4d4d';
+      elStyle.style.border = '1px solid #ced4da !important';
+
+      elRmStyle.style.background = 'linear-gradient(to right, #ce8149, #ce8149)';
+      elRmStyle.style.color = '#fff';
+      elRmStyle.style.border = 'none';
+
+      prInst.style.display = 'block';
+    }
   }
 
   openClickMenu() { openMenu();}
